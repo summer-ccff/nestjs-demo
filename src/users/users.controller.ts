305533afee7b0fb204c18b5dc9dce3c46b367828
Fengwu Guo <guofengwu1994@163.com>
2026-04-25 07:19:37 +0800

    添加用户控制器和服务，实现用户的增查功能

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
+import { Body, Get, Param, Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: string) {
+    return this.usersService.findOne(Number(id));
+  }
+
+  @Post()
+  createUser(@Body() body: { name: string }) {
+    return this.usersService.create(body.name);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    { id: 1, name: 'Tom' },
+    { id: 2, name: 'Jerry' },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  create(name: string) {
+    const newUser = {
+      id: this.users.length + 1,
+      name,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
